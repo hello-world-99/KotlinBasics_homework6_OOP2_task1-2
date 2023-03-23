ecc7e05f4a98670d0c48e5388e618e886c514ea9
hello-world-99 <69188993+hello-world-99@users.noreply.github.com>
2023-03-23 11:04:29 +0300

    test

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,5 +1,4 @@
-import Post.Attachment.Attachment
-import Post.Attachment.Audio
+import Post.Attachment.*
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
@@ -44,11 +43,39 @@ class WallServiceTest {
     }
 
     @Test
-    fun attachment(){
+    fun attachment1(){
         val newPost=WallService.original.copy()
         newPost.attachments+=Audio(1,1,"1","1",100,"www.music.com",1672520400)
         wall.add(newPost)
         assertEquals(newPost.attachments.last().type=="Audio",true)
+    }
+    @Test
+    fun attachment2(){
+        val newPost=WallService.original.copy()
+        newPost.attachments+=Video(1,1,"1",100,"1",1672520400,1,1)
+        wall.add(newPost)
+        assertEquals(newPost.attachments.last().type=="Video",true)
+    }
+    @Test
+    fun attachment3(){
+        val newPost=WallService.original.copy()
+        newPost.attachments+=Document(1,1,"1",1,"png",1672520400)
+        wall.add(newPost)
+        assertEquals(newPost.attachments.last().type=="Document",true)
+    }
+    @Test
+    fun attachment4(){
+        val newPost=WallService.original.copy()
+        newPost.attachments+=Photo(1,1,1,1,"100",1672520400,100,100)
+        wall.add(newPost)
+        assertEquals(newPost.attachments.last().type=="Photo",true)
+    }
+    @Test
+    fun attachment5(){
+        val newPost=WallService.original.copy()
+        newPost.attachments+=Product(1,1,"adssda","sdadasd",100)
+        wall.add(newPost)
+        assertEquals(newPost.attachments.last().type=="Product",true)
     }
 
     @Before
